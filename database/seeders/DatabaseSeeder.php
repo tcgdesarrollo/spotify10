54bbfa6aaed6bf0e29ee2545d836c43a001202f7
Arzuaga <alberto.arzuaga@thecloud.group>
2024-03-24 11:01:59 -0400

    Arregla el databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(
+        $this->call([
             ChartSeeder::class,
             VersionSeeder::class
-        );
+        ]);
     }
 }
