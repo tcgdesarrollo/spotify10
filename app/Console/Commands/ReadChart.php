a8edda0d63105b2e3827ad8c33294e2d6aae8780
Arzuaga <alberto.arzuaga@thecloud.group>
2024-03-24 21:30:58 -0400

    Agrega cron para cargar el historial

--- app/Console/Commands/ReadChart.php
+++ app/Console/Commands/ReadChart.php
@@ -88,11 +88,12 @@ class ReadChart extends Command
                     "singer" => $singer
                 ]
             );
-            foreach (ChartItem::where('chart_date_id', $chart_date->id)->orderBy('position')->take(2)->get() as $item){
-                (new TelegramMessageController())->store("$item->position. $item->title - $item->singer");
-            }
+
 //            $this->comment("$position. $title - $singer ($last $peak $weeks)");
         });
+        foreach (ChartItem::where('chart_date_id', $chart_date->id)->orderBy('position')->take(2)->get() as $item) {
+            (new TelegramMessageController())->store("$item->position. $item->title - $item->singer");
+        }
 
     }
 
