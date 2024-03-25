6188055aa5ea69ce12bc8413fc78f6c1053469ce
Arzuaga <alberto.arzuaga@thecloud.group>
2024-03-24 21:13:40 -0400

    Agrega cron para cargar el historial

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
     {
          $schedule->command('app:read-chart')->cron('0 8,12 * * *');
          $schedule->command('send:telegram')->cron('* * * * *');
+         $schedule->command('app:read-history')->cron('20 21 * * 7')->withoutOverlapping(120);
     }
 
     /**
