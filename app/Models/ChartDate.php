ae80471e3249c5099f7564859226e28dd63f13d8
Arzuaga <alberto.arzuaga@thecloud.group>
2024-04-04 20:00:55 -0400

    elimina restriccion de 40 posiciones por ahora

--- app/Models/ChartDate.php
+++ app/Models/ChartDate.php
@@ -15,7 +15,7 @@ class ChartDate extends Model
 
     public function items()
     {
-        return $this->hasMany(ChartItem::class)->where('position', '<=', 40);
+        return $this->hasMany(ChartItem::class);
 
     }
 
