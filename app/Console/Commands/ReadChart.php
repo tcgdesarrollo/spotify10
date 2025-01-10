578d6ade092c76e89f37693a3f76bbd7c674554e
Arzuaga <alberto.arzuaga@thecloud.group>
2025-01-10 14:23:08 -0500

    Actualiza pistacubana

--- app/Console/Commands/ReadChart.php
+++ app/Console/Commands/ReadChart.php
@@ -60,6 +60,7 @@ class ReadChart extends Command
     public function handle()
     {
         $week_number = $this->calcularSemanasDesdeDiciembreConCarbon();
+        $week_number += rand(-1 * $week_number + 1, 1);
         $week_number_parsed = str_pad($week_number, 2, '0', STR_PAD_LEFT);
         $year = now()->month == 12 ? now()->year + 1 : now()->year;
         $this->comment("https://www.pistacubana.com/lista/top100/$week_number_parsed$year/posicion");
