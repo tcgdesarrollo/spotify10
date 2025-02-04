23a70d624a7bcc39b3611eb49ee0dad100af5b56
Arzuaga <alberto.arzuaga@thecloud.group>
2025-02-04 08:38:33 -0500

    Agrega el registro obligatorio

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+
+Schedule::command('app:read-chart')->cron('*/30 * * * *');
+Schedule::command('send:telegram')->cron('* * * * *');
+Schedule::command('run:tools')->cron('0 * * * *');
