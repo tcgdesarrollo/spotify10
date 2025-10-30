8fc471761cce68160e9e5ea548c56b02191e6e8e
Arzuaga <alberto.arzuaga@thecloud.group>
2025-10-30 12:09:09 -0400

    Actualiza parser de Billboard para mejor manejo de datos.

--- app/Models/ChartItem.php
+++ app/Models/ChartItem.php
@@ -10,13 +10,22 @@ class ChartItem extends Model
 {
     protected $guarded = ['id'];
     protected $hidden = ['created_at', 'updated_at'];
+
 //    protected $appends = ['fulltitle'];
+
+
+    public function chart_date()
+    {
+        return $this->belongsTo(ChartDate::class);
+
+    }
 
     public function getfulltitleAttribute()
     {
         $title = "$this->position. $this->title  $this->singer ($this->last_position $this->peak_position $this->week_on_chart)";
-//        if (isset($this->streams))
-//            $title.= " (Streams: ".number_format($this->streams, 0, ',', '.').")";
+        $chart = $this->chart_date->chart;
+        if (isset($this->streams) && str_contains($chart->name, 'Global Daily'))
+            $title .= " (Streams: " . number_format($this->streams, 0, ',', '.') . ")";
         return $title;
 
     }
